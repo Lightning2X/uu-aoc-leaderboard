28ee9b753a9c39589599c6ca7550ffd435573524
Tim Schagen <schagen34@gmail.com>
2021-11-23 22:03:41 +0100

    Move test/fakeData to seperate file

--- src/components/table/leaderboard-table.tsx
+++ src/components/table/leaderboard-table.tsx
@@ -10,6 +10,7 @@ import {
 } from "@material-ui/core";
 import _ from "lodash";
 import React from "react";
+import { testData } from "../../shared/testData";
 import { calculateScores } from "./calculatescores";
 import {
   miliSecondTableFormatter,
@@ -39,73 +40,6 @@ const columns: readonly Column[] = [
     label: "Total time",
     minWidth: 75,
     format: miliSecondTableFormatter,
-  },
-];
-
-const fakeData: APIData[] = [
-  {
-    year: 2020,
-    day: 1,
-    username: "Rutgerdj",
-    startTime: "2021-11-22T20:09:30.410Z",
-    starOne: "2021-11-22T20:14:10.663Z",
-    starTwo: "2021-11-22T20:15:13.987Z",
-  },
-  {
-    year: 2020,
-    day: 3,
-    username: "Rutgerdj",
-    startTime: "2021-11-22T20:28:28.960Z",
-    starOne: null,
-    starTwo: null,
-  },
-  {
-    year: 2020,
-    day: 2,
-    username: "Rutgerdj",
-    startTime: "2021-11-22T20:15:37.987Z",
-    starOne: "2021-11-22T20:19:19.487Z",
-    starTwo: null,
-  },
-  {
-    year: 2020,
-    day: 6,
-    username: "Rutgerdj",
-    startTime: "2021-11-22T20:42:54.260Z",
-    starOne: null,
-    starTwo: null,
-  },
-  {
-    year: 2020,
-    day: 1,
-    username: "Lightning2x",
-    startTime: "2021-11-22T20:09:30.410Z",
-    starOne: "2021-11-22T20:15:10.663Z",
-    starTwo: "2021-11-22T20:16:13.987Z",
-  },
-  {
-    year: 2020,
-    day: 3,
-    username: "Lightning2x",
-    startTime: "2021-11-22T20:28:28.960Z",
-    starOne: "2021-11-22T21:15:10.663Z",
-    starTwo: null,
-  },
-  {
-    year: 2020,
-    day: 2,
-    username: "Lightning2x",
-    startTime: "2021-11-22T20:15:37.987Z",
-    starOne: "2021-11-22T21:19:19.487Z",
-    starTwo: "2021-11-22T21:25:09.290Z",
-  },
-  {
-    year: 2020,
-    day: 6,
-    username: "Lightning2x",
-    startTime: "2021-11-22T20:42:54.260Z",
-    starOne: null,
-    starTwo: null,
   },
 ];
 
@@ -226,7 +160,7 @@ function LeaderBoardTable() {
             </TableRow>
           </TableHead>
           <TableBody className={styles["theme-table-body"]}>
-            {getRowsJSX(mapData(fakeData))}
+            {getRowsJSX(mapData(testData))}
           </TableBody>
         </Table>
       </TableContainer>
---
+++ src/shared/testData.tsx
@@ -0,0 +1,68 @@
+import { APIData } from "../components/table/leaderboard-table.types";
+
+export const testData: APIData[] = [
+  {
+    year: 2020,
+    day: 1,
+    username: "Rutgerdj",
+    startTime: "2021-11-22T20:09:30.410Z",
+    starOne: "2021-11-22T20:14:10.663Z",
+    starTwo: "2021-11-22T20:15:13.987Z",
+  },
+  {
+    year: 2020,
+    day: 3,
+    username: "Rutgerdj",
+    startTime: "2021-11-22T20:28:28.960Z",
+    starOne: null,
+    starTwo: null,
+  },
+  {
+    year: 2020,
+    day: 2,
+    username: "Rutgerdj",
+    startTime: "2021-11-22T20:15:37.987Z",
+    starOne: "2021-11-22T20:19:19.487Z",
+    starTwo: null,
+  },
+  {
+    year: 2020,
+    day: 6,
+    username: "Rutgerdj",
+    startTime: "2021-11-22T20:42:54.260Z",
+    starOne: null,
+    starTwo: null,
+  },
+  {
+    year: 2020,
+    day: 1,
+    username: "Lightning2x",
+    startTime: "2021-11-22T20:09:30.410Z",
+    starOne: "2021-11-22T20:15:10.663Z",
+    starTwo: "2021-11-22T20:16:13.987Z",
+  },
+  {
+    year: 2020,
+    day: 3,
+    username: "Lightning2x",
+    startTime: "2021-11-22T20:28:28.960Z",
+    starOne: "2021-11-22T21:15:10.663Z",
+    starTwo: null,
+  },
+  {
+    year: 2020,
+    day: 2,
+    username: "Lightning2x",
+    startTime: "2021-11-22T20:15:37.987Z",
+    starOne: "2021-11-22T21:19:19.487Z",
+    starTwo: "2021-11-22T21:25:09.290Z",
+  },
+  {
+    year: 2020,
+    day: 6,
+    username: "Lightning2x",
+    startTime: "2021-11-22T20:42:54.260Z",
+    starOne: null,
+    starTwo: null,
+  },
+];
