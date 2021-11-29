b09173c7c49a76931401fbe18b1a405781820ee9
Tim Schagen <schagen34@gmail.com>
2021-11-29 16:31:04 +0100

    Add persistence for year

--- src/app.store.ts
+++ src/app.store.ts
@@ -1,12 +1,13 @@
 import { asGlobalStore } from "shared/stores"
 
 const createStore = () => ({
-  year: new Date().getUTCFullYear(),
+  year: localStorage.getItem("year") ? Number(localStorage.getItem("year")) : new Date().getUTCFullYear(),
   setYear(year: number) {
       if(year < 2015 || year > new Date().getUTCFullYear()) {
           return;
       }
       this.year = year;
+      localStorage.setItem("year", year.toString());
   }
 });
 
